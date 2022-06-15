cb8d68a9d30801b18ce82bc18b46b21fe61dd96d
Orange <>
2022-06-15 20:17:58 +0800

    fix:bid price

---
+++ contracts/interfaces/XmobExchangeCoreInterface.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface XmobExchangeCoreInterface 
+{    
+    function amountTotal() external view returns(uint256);
+
+    function joinPay(address member) payable external;
+}
+
